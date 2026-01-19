3228e196e144ac27e1193ae75536dc1a1a662764
Sabrey <limsabriey94@gmail.com>
2026-01-19 17:32:48 +0700

    update

--- Ecommerce/resources/views/About/About.blade.php
+++ Ecommerce/resources/views/About/About.blade.php
@@ -3,7 +3,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("This is About") }}
+                    <div class="text-center">
+                        <p>Our Story</p>
+                        <p>Crafting Premium Shopping <br> Experiences</p>
+                        <p>Founded in 2014, Luxe has been at the forefront of premium e-commerce, curating exceptional products <br> that blend quality, style, and innovation.</p>
+                    </div>
                 </div>
             </div>
         </div>
